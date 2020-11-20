aa5296fa8a3958ea1ce78e9b03aaa8eca00ff4c8
sunshine <pasichnykolena1998@gmail.com>
2020-11-20 21:22:30 +0200

    created c message, message.service.ts and hero.service.ts

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
-import {HEROES} from '../name-heroes';
+
 import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
+import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,15 +11,22 @@ import {Hero} from '../hero';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-selectedHero: Hero;
-  constructor() { }
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService, private messageService: MessageService) {
+  }
 
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}\`);`)
   }
 }
-
