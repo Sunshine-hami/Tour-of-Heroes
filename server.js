f2d6c32bb8df16d7381b478c8b53dd93283077aa
sunshine <pasichnykolena1998@gmail.com>
2020-12-04 20:24:58 +0200

    upd

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(`${__dirname}/dist/app-Tour-of-Heroes` ));
 // send all requests to index.html
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/app-Tour-of-Heroes/index.html` ));
+  res.sendFile(path.join(`${__dirname}/dist/app-Tour-of-Heroes/index.html` ));
 });
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
